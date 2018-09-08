0f47d333928de9d846eb085fe4961784d93cace9
MY <353991880@qq.com>
2018-09-08 19:26:54 +0800

    Initial commit

---
+++ src/Sum.java
@@ -0,0 +1,14 @@
+public class Sum {
+    public int[] twoSum1(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+//        throw new IllegalArgumentException("No two sum solution");
+        return nums;
+    }
+
+}
